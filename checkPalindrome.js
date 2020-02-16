943e7156eee0869ddac63bc79369996af625a6b7
zbalch <57383680+zbalch@users.noreply.github.com>
2020-02-16 00:19:49 -0600

    Initial upload

---
+++ checkPalindrome.js
@@ -0,0 +1,15 @@
+function checkPalindrome(inputString) {
+    const revString = inputString.split('').reverse().join('');
+
+    if (inputString === revString) {
+        console.log(true);
+        return true;
+    }
+    else {
+        console.log(false);
+        return false;
+    }
+}
+
+checkPalindrome('bolton'); //expected output: false
+
